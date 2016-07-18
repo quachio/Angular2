ddae4402a4ac7bd4d3a85763ef934b0a16509145
Hung Quach <quachio@icloud.com>
2016-07-17 18:42:26 -0700

    modified tsconfig.json to igmore some file

---
+++ my-project/src/main.ts
@@ -0,0 +1,10 @@
+// main.ts
+import { bootstrap }                 from '@angular/platform-browser-dynamic';
+import { enableProdMode }            from '@angular/core';
+import { AppComponent, environment } from './app/';
+
+if (environment.production) {
+  enableProdMode();
+}
+
+bootstrap(AppComponent);
